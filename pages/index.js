d59c15355fee3140b7073b9e98a41698fc98f368
truongthimyhao <truongthimyhao4295@gmail.com>
2022-06-13 15:29:53 +0700

    config file .eslintrc.json

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       hello my-next-app...1
     </div>
   )
