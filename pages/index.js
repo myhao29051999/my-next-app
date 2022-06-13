6a84f7c90e69c94009eeb84a109288b8822983ed
truongthimyhao <truongthimyhao4295@gmail.com>
2022-06-13 15:08:59 +0700

    combination of 3 commit
    second commit
    
    config file .eslintrc.json
    
    hhh

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-      hello my-next-app
+    <div>
+      hello my-next-app...
     </div>
   )
 }
